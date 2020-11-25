98f033e06d1379636f029457263bda5ea53d505e
Dion Recai <dijereo909@gmail.com>
2020-11-24 22:53:54 -0400

    Location of user is stored in firestore

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { GeolocationPosition, Plugins } from '@capacitor/core';
+import { FirebaseService } from 'src/app/services/firebase.service';
+
 const { Geolocation } = Plugins;
 
 @Injectable({
@@ -7,19 +9,21 @@ const { Geolocation } = Plugins;
 })
 export class LocationService {
   coords: any;
-  constructor() { }
+  constructor(public userid: string, public fireServ: FirebaseService) { }
 
   async locate() {
     const coordinates = await Geolocation.getCurrentPosition({'maximumAge': 10, 'enableHighAccuracy': true});
     this.coords = coordinates.coords;
     console.log(this.coords);
+    this.fireServ.collectionName = "Users";
+    this.fireServ.update_record(this.userid, {location: coordinates});
   }
 
   degreesToRadians(degrees: number) {
     return degrees * Math.PI / 180;
   }
 
-  distanceToCoordinateInMetres(targetCoords: GeolocationPosition) {
+  distanceToCoordinateInMetres(targetCoords: GeolocationPosition): number {
     let earthRadiusMetres = 6371000;
     let deltaLat = this.degreesToRadians(targetCoords.coords.latitude - this.coords.latitude);
     let deltaLon = this.degreesToRadians(targetCoords.coords.latitude - this.coords.latitude);
@@ -30,7 +34,7 @@ export class LocationService {
     return 2 * earthRadiusMetres * Math.atan2(Math.sqrt(temp), Math.sqrt(1-temp));
   }
 
-  withinRange(targetCoords: GeolocationPosition, metres: number) {
+  withinRange(targetCoords: GeolocationPosition, metres: number): boolean {
     return this.distanceToCoordinateInMetres(targetCoords) <= metres;
   }
 }
