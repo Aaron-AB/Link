d7ca76a4a987d294a1773dff9b6b28d5b9b90cd1
kjgillll <keronn.gill@my.uwi.edu>
2020-11-29 19:12:26 -0400

    Firebase User Object Get

--- src/app/swiper-gesture/swiper-gesture.page.ts
+++ src/app/swiper-gesture/swiper-gesture.page.ts
@@ -7,48 +7,54 @@ import {
   AfterViewInit,
 } from "@angular/core";
 import { GestureCtrlService } from "src/app/providers/gestureCtrl-service/gesture-ctrl.service";
-import { IonCard } from "@ionic/angular";
-
+import { IonCard, Platform } from "@ionic/angular"; 
+import * as firebase from 'firebase'; 
+import { FirebaseService } from '../../app/services/firebase.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import {map} from 'rxjs/operators';
 @Component({
   selector: "app-swiper-gesture",
   templateUrl: "./swiper-gesture.page.html",
   styleUrls: ["./swiper-gesture.page.scss"],
 })
 export class SwiperGesturePage implements OnInit, AfterViewInit {
-  people = [
-    {
-      id: "0",
-      name: "Ajay",
-      img:
-        "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-      power: 0,
-    },
-    {
-      id: "0",
-      name: "Lion",
-      img:
-        "https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-      power: 0,
-    },
-    {
-      id: "0",
-      name: "Tree",
-      img:
-        "https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-      power: 0,
-    },
-  ];
+  //people = [];  
+  
+  posts=[];
 
   @ViewChildren(IonCard, { read: ElementRef }) cards: QueryList<ElementRef>;
 
-  constructor(private gestureCtrlService: GestureCtrlService) {}
+  constructor(private gestureCtrlService: GestureCtrlService ,
+    private platform: Platform,
+    private sanitizer: DomSanitizer, 
+    public fireService : FirebaseService, 
+    public router: Router) {}
 
-  ngOnInit() {
-    console.log("people", this.people);
+  ngOnInit() {  
+    this.fireService.read_record("Users").subscribe(data => {
+
+      this.posts = data.map(e => {
+    
+        return {
+          //id: e.payload.doc.id,
+          Name: e.payload.doc.data()['name'],
+          Email: e.payload.doc.data()['email'], 
+          ProfilePicture: e.payload.doc.data()['profilePicture'], 
+        };
+    
+      })
+      console.log(this.posts);
+     });
+
   }
+
+  
 
   ngAfterViewInit() {
     const cardArray = this.cards.toArray();
     this.gestureCtrlService.useSwiperGesture(cardArray);
-  }
-}
+  } 
+
+
+}//end export class()
